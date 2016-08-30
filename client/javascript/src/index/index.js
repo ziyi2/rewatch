94ddf65c78d750f84ed07bf7df7b7c2fbf20f259
zhuxiankang <673191402@qq.com>
2016-08-30 18:55:15 +0800

    modify webpack.browser.config.js

--- client/javascript/src/index/index.js
+++ client/javascript/src/index/index.js
@@ -1,5 +1,7 @@
 
-import './components/TodoList.jsx';
+//import 'babel-polyfill'; //如果不需要特殊API,不用增加负担,后端没关系随时可以加载
+
+import './components/TodoList';
 
 //require('./components/TodoList');
 
--- webpack/dev/webpack.browser.config.js
+++ webpack/dev/webpack.browser.config.js
@@ -95,8 +95,12 @@ var webpack = require('webpack');
 var path = require('path');
 var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
 
+console.log('webpack.browser.config start running...' );
 
 module.exports = {
+
+    target: 'web',
+
     entry: {
         index: './client/javascript/src/index/index.js'
     },
@@ -111,14 +115,43 @@ module.exports = {
         'react-dom': 'ReactDOM'
     },
 
+    resolve: {      //require的时候可以省略后缀
+        extensions: ['', '.js', '.jsx']
+    },
+
+    //context: __dirname,
+    //
+    //node: {
+    //    __filename: false,
+    //    __dirname: false
+    //},
+
     module: {
         loaders: [
             {
-                test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+
+                test: /\.js[x]?$/,
+                exclude: /node_modules/,
+
+                //exclude: [
+                //    path.resolve(__dirname, "node_modules")
+                //],   //调试两天的问题出不来就出在这里!
+
+
+                //霄哥的配置项
+                //loader: 'babel-loader?presets[]=es2015&presets[]=react'
+
+                //我的配置项
+                loader: 'babel-loader',
+                query: {
+                    //plugins: ['transform-runtime'],  //此项虽然对于编译代码非常有些帮助,但是会产生额外的重复负担代码
+                    presets:['es2015','stage-0','react']  //此项可以省略而是添加在.babelrc文件,但是官方建议保留在这里
+                }
+            }
         ]
     },
 
-    plugins: [commonsPlugin] //打包多个入口文件时会提取出公用的部分，生成common.js
+    plugins: [commonsPlugin]   //打包多个入口文件时会提取出公用的部分，生成common.js
 };
 
 
